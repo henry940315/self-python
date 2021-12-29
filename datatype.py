2f53b12121eff2164050fb44c8f79cf0e605a940
Henry <henry940315@gmail.com>
2021-12-29 12:04:57 +0800

    ff

---
+++ datatype.py
@@ -0,0 +1,17 @@
+#布林值
+True 
+False
+#有順序 可變動的列表 List
+[3,4,5]
+["Amy","Mike"]
+#有順序 不可變動的列表 Tuple
+(3,4,5)
+("Amy","Mike")
+#集合
+{3,4,5}
+{"Amy","Mike"}
+#字典
+{"Hans":"Jons","Rus":"Pens"}
+{"Hans":5,"Rus":8}
+#變數名稱 = 資料
+x = 3
